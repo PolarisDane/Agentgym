3fcd4373bee4cdc86729772d90f8b2466969b93b
Wang Ziyu <1172789766@qq.com>
2026-05-01 14:13:28 +0000

    Save current state before merging wmc_erc.py changes

--- agentenv-alfworld/setup.sh
+++ agentenv-alfworld/setup.sh
@@ -1,5 +1,5 @@
 pip install alfworld==0.3.3
 pip uninstall opencv-python -y
 pip install -e .
-export ALFWORLD_DATA=~/.cache/alfworld
+export ALFWORLD_DATA=/inspire/hdd/project/robot-reasoning/xuyue-p-xuyue/ziyu/.cache/alfworld
 alfworld-download
